473c05abd367aa4d3ddef2c404ed38da7d6f529e
Tan Hu Soon <62252682+husoonnn@users.noreply.github.com>
2023-05-06 12:38:41 +0800

    auth and erros BE

--- backend/auth.py
+++ backend/auth.py
@@ -1,24 +1,40 @@
 from flask import Flask, request, jsonify
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from errors import error_response
+from flask_sqlalchemy import SQLAlchemy
+from app import Employee
 
 app = Flask(__name__)
-app.config['JWT_SECRET_KEY'] = 'super-secret'
 jwt = JWTManager(app)
+app.config['JWT_SECRET_KEY'] = 'super-secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:admin@localhost:3306/expenseclaimsdata'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 @app.route('/login', methods=['POST'])
 def login():
-    username = request.json.get('username')
+    
+    id = request.json.get('id')
     password = request.json.get('password')
-    if not username or not password:
-        return jsonify({'msg': 'Username and password required'}), 400
+    employee = db.get_user_by_id(id)
+    employee = Employee.query.filter_by(EmployeeID=id)
+    if not id or not password:
+        return error_response(400, 'Employee ID and password required')
+    
+    if not id or employee.password != password:
+        return error_response(401, "Employee ID or password invalid.")
 
+    firstName = employee.firstname
+    lastName = employee.lastName
+    accountNumber = employee.bank_account_no
+    
     # Check if the user exists in the database and the password is correct
     # If successful, create an access token and return it to the client
-    access_token = create_access_token(identity=username)
-    return jsonify({'access_token': access_token}), 200
+    access_token = create_access_token(identity=id)
+    return jsonify({'access_token': access_token, 'firstName': firstName, 'lastName': lastName, 'accountNumber' : accountNumber}), 200
 
 @app.route('/protected')
 @jwt_required()
 def protected():
-    username = get_jwt_identity()
-    return jsonify({'username': username}), 200
+    id = get_jwt_identity()
+    return jsonify({'employeeId': id}), 200
