47eab34323cd0861bd5c1107ce6bf338c14365ce
Bryan Personal <bryanlohjw97@gmail.com>
2023-05-06 14:31:12 +0800

    feat: header with links

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,26 @@
+import { Outlet, Link } from 'react-router-dom';
+import classes from './Header.module.css';
+
+const Header = () => {
+	return (
+		<div className={classes.body}>
+			<div className={classes.header}>
+				<div className={classes.logo}></div>
+				<div className={classes.links}>
+					<Link to={'/dashboard'} className={classes.action}>
+						<p>Home</p>
+					</Link>
+					<Link to={'/createClaim'} className={classes.action}>
+						<p>Create Claim</p>
+					</Link>
+					<Link to={'/'} className={classes.action}>
+						<p>Logout</p>
+					</Link>
+				</div>
+			</div>
+			<Outlet />
+		</div>
+	);
+};
+
+export default Header;
