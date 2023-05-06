d59d24135ce088d3b112290998507182d64bfbdb
Yeo H.X <y.huixin@yahoo.com.sg>
2023-05-06 14:32:32 +0800

    Add login route

--- backend/app.py
+++ backend/app.py
@@ -3,8 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, unset_jwt_cookies
 from uuid import uuid4
 import json
+from errors import error_response
 
 app = Flask(__name__)
+jwt = JWTManager(app)
+app.config['JWT_SECRET_KEY'] = 'super-secret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:admin@localhost:3306/expenseclaimsdata'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -132,6 +135,36 @@ def update_claim(claim_id):
     db.session.commit()
 
     return jsonify({"message": "Expense updated"})
+
+@app.route('/login', methods=['POST'])
+def login():
+    
+    id = request.json.get('id')
+    password = request.json.get('password')
+    
+    employee = Employee.query.get(id)
+    print(employee)
+    if not id or not password:
+        return error_response(400, 'Employee ID and password required')
+    
+    if not id or employee.Password != password:
+        return error_response(401, "Employee ID or password invalid.")
+
+    firstName = employee.FirstName
+    lastName = employee.LastName
+    accountNumber = employee.BankAccountNumber
+    
+    # Check if the user exists in the database and the password is correct
+    # If successful, create an access token and return it to the client
+    access_token = create_access_token(identity=id)
+    return jsonify({'access_token': access_token, 'firstName': firstName, 'lastName': lastName, 'accountNumber' : accountNumber}), 200
+
+@app.route('/protected')
+@jwt_required()
+def protected():
+    id = get_jwt_identity()
+    return jsonify({'employeeId': id}), 200
+
 
 
 
